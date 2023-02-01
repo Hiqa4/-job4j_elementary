0003bfc34e96f1554702b52904cb7775cc512f6f
Mihail <mihail.krasun@mail.ru>
2023-02-01 20:48:48 +0400

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Mihail Krasun");
         System.out.println("24.09.2000");
     }
