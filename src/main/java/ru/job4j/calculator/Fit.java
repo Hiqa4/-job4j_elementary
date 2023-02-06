fbfd7cc9acf9d3641263d1edd63bf0afdffd8167
Mihail <mihail.krasun@mail.ru>
2023-02-07 03:24:35 +0400

    6. Идеальный вес [#41584 #389011] - замена переменных на "return".

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,13 +3,11 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        double resultMan = (height - 100) * 1.15;
-        return resultMan;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWight(short height) {
-        double resultWoman = (height - 110) * 1.15;
-        return resultWoman;
+       return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
